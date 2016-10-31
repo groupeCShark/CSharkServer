1452291ba7667f490c4cadf8a061c93aafccfddd
Jeremy Marin <jeremy.marin@student.ecp.fr>
2016-10-31 10:59:11 +0100

    Basic server implementation

---
+++ CSharkServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace CSharkServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharkServiceImpl csharkService = new CSharkServiceImpl();
+            ServiceHost host = new ServiceHost(csharkService);
+
+            host.Open();
+
+            Console.WriteLine("Server CShark is running...");
+            Console.WriteLine("Press any Key to exit...");
+            Console.ReadKey();
+
+            host.Close();
+        }
+    }
+}
